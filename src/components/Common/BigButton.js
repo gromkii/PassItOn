35042a30471787b81b827844017a4d580c0eb71e
Dax <limeside@gmail.com>
2018-02-19 22:20:21 -0600

    Edit BigButton styling. TouchableOpacity width = button width now. :rabbit:

--- src/components/Common/BigButton.js
+++ src/components/Common/BigButton.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { TouchableOpacity, View, StyleSheet } from 'react-native';
 
-// TODO: Touchable is full width, just the view needs to be that long. Contain Touchable in view.
-
 const BigButton = props => {
   return (
-    <View style={style.button}>
-
-    <TouchableOpacity onPress={props.onPress} style={style.button}>
-      <View style={style.container}>
-        {props.children}
-      </View>
-    </TouchableOpacity>
+    <View style={style.bigContainer}>
+      <TouchableOpacity onPress={props.onPress} style={style.button}>
+        <View style={style.container}>
+          {props.children}
+        </View>
+      </TouchableOpacity>
     </View>
   )
 };
 
 const style = StyleSheet.create({
+  bigContainer: {
+    width: "100%",
+    alignItems:"center"
+  },
   button: {
-    width: "80%",
+    width: "60%",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -30,7 +31,7 @@ const style = StyleSheet.create({
     alignItems:"center",
     justifyContent:"center",
     borderRadius:5,
-    width: "80%"
+    width: "100%"
   }
 });
 
